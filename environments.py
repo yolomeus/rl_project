f885b1c233b7802492f24699b56faef21385dc36
Fabian <fab.beringer@gmail.com>
2021-02-04 17:19:58 +0100

    add player class

--- environments.py
+++ environments.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+from collections import defaultdict
+
 from gym import Env
 from gym.spaces import Discrete
 
@@ -9,9 +12,14 @@ class Expando(Env):
         # TODO compute n_buildings_types internally
         self.action_space = Discrete(n_actions)
         obs_dims = grid_size + ((n_building_types + 1) * n_players,)  # +1 for empty position
+        self.observation_space = OneHot(obs_dims)
+
+        self.players = [Player(i) for i in range(n_players)]
 
     def step(self, actions):
-        pass
+        assert len(actions) == len(self.players)
+        for player, action in zip(actions, self.players):
+            player.act(action)
 
     def reset(self):
         pass
@@ -25,3 +33,15 @@ class Expando(Env):
         for building in player.buildings:
             all_rewards.append(building.compute_reward())
         return sum(all_rewards)
+
+
+class Player:
+    def __init__(self, player_id):
+        self.player_id = player_id
+        self.buildings = []
+
+    def act(self, action):
+        pass
+
+    def register_building(self, building):
+        self.buildings.append(building)
