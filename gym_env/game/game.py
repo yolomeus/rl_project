2ca3080e8db679f3548e5e261ecb15be44afccca
Fabian <fab.beringer@gmail.com>
2021-02-08 20:39:18 +0100

    refactor: move action decoding logic to game class

--- gym_env/game/game.py
+++ gym_env/game/game.py
@@ -1,26 +1,32 @@
+import numpy as np
+
 from gym_env.game.board import Board
 from gym_env.game.player import Player
 
 
 class ExpandoGame:
-    def __init__(self, grid_size, n_players):
+    def __init__(self, grid_size, n_players, building_types):
+        self.grid_size = grid_size
+        self.n_dims = len(self.grid_size)
         self.n_players = n_players
-        self.board = Board(grid_size)
+        self.board = Board(grid_size, building_types)
         self.players = [Player(i, self.board) for i in range(n_players)]
         self.n_turns = 0
 
     def take_turn(self, action, player_id):
-        self.execute(action, player_id)
+        cursor_move, place_action = action
+        move_direction = self._decode_action(cursor_move, 'cursor_move')
+        place_action = self._decode_action(cursor_move, 'place_action')
+
+        self.players[player_id].move_cursor(move_direction)
+
         reward = self.players[player_id].current_reward
         self.n_turns += 1
         return reward
 
-    def execute(self, action, player_id):
-        pass
-
     def reset(self):
         self.n_turns = 0
-        self.board.clear()
+        self.board.reset_grid()
 
     def get_observation(self, player_id):
         return self.players[player_id].get_observation()
@@ -28,3 +34,36 @@ class ExpandoGame:
     @property
     def is_done(self):
         return False
+
+    def _decode_action(self, action, action_type):
+        if action_type == 'cursor_move':
+            return self._decode_cursor_move(action)
+        elif action_type == 'place_action':
+            return self._decode_place_action(action)
+        else:
+            raise NotImplementedError()
+
+    def _decode_cursor_move(self, action: int):
+        """Take an integer and return the corresponding direction vector for moving the cursor. The integer encoding
+        for a cursor move is within range [0, 2*n_dims] where 0 means no change in direction, 1 <= k <= n_dims means +1
+        along the k-th dimension and n_dims+1 <= i <= 2*n_dims means -1 along the (i-n_dims)-th dimension.
+
+        :param action: integer representing cursor move
+        :return: direction vector to move to as a numpy array
+        """
+        assert action >= 0
+        assert 0 <= action <= 2 * self.n_dims, 'we can move in two directions for each axis or not move at all'
+
+        direction = np.zeros((self.n_dims,))
+        if 0 < action <= self.n_dims:
+            # 1 to n_dimensions
+            direction[action - 1] += 1
+        elif self.n_dims < action <= 2 * self.n_dims:
+            # n_dimensions to 2 * n_dimensions
+            direction[(action + 1) % self.n_dims] -= 1
+        # else action=0, do nothing...
+
+        return direction
+
+    def _decode_place_action(self, action):
+        return self.n_players
--- gym_env/game/player.py
+++ gym_env/game/player.py
@@ -16,48 +16,15 @@ class Player:
         self.cursor = np.zeros(len(self.board.grid_size))
 
     def move_cursor(self, direction):
-        """
-
-        :param direction:
-        :return:
-        """
-        assert direction >= 0
-        assert 0 <= direction <= 2 * len(self.cursor), 'we can move in two directions for each axis or not move at all'
-
-        cur_pos = self.cursor.copy()
-        n_dimensions = len(self.cursor)
-        if 0 < direction <= n_dimensions:
-            # 1 to n_dimensions
-            cur_pos[direction - 1] += 1
-        elif len(self.cursor) < direction <= 2 * len(self.cursor):
-            # n_dimensions to 2 * n_dimensions
-            cur_pos[(direction + 1) % n_dimensions] -= 1
-        # else do nothing...
-
-        is_legal_move = self.is_legal(cur_pos)
-        if is_legal_move:
-            self.cursor = cur_pos
-
-        return is_legal_move
-
-    def is_legal(self, position):
-        """
-
-        :param position:
-        :return:
-        """
-        for i, x in enumerate(position):
-            if x < 0 or x >= self.board.grid_size[i]:
-                return False
-
-        return True
+        if self.board.is_legal_position(self.cursor + direction):
+            self.cursor += direction
 
     def register_building(self, building: Building):
         self.buildings.append(building)
 
     def get_observation(self):
         # this is where we control observability
-        return self.board.to_binary(), self.cursor
+        return self.board.to_one_hot(), self.cursor
 
     @property
     def current_reward(self):
