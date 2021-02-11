3eb97afc1efec65ca0586446c6b1e587da1c158e
Fabian <fab.beringer@gmail.com>
2021-02-11 19:31:07 +0100

    implement one hot encoding for board

--- gym_env/game/board.py
+++ gym_env/game/board.py
@@ -1,24 +1,34 @@
+import itertools
 from collections import defaultdict
+
+import numpy as np
+
+from gym_env.game.buildings import Empty
 
 
 class Board:
     def __init__(self, grid_size, game):
+        self.grid_size = grid_size
         self.game = game
         self.type_to_id = {t: i for i, t in enumerate(game.building_types)}
-        self.grid_size = grid_size
+        # shared empty field
+        self.empty_field = Empty(None, self)
         self.grid = None
         self.reset_grid()
+
+        self.one_hot_dim = game.n_players * len(game.building_types)
+        self._one_hots = np.eye(self.one_hot_dim)
 
     def get_piece(self, coordinates):
         return self.grid[coordinates]
 
     def place_piece(self, piece, coordinates):
         assert self.is_legal_position(coordinates), 'the coordinates provided are off the grid'
-        assert self.grid[coordinates] is None, 'can only place piece on empty field'
+        # assert self.grid[coordinates] is self.empty_field, 'can only place piece on empty field'
         self.grid[coordinates] = piece
 
     def reset_grid(self):
-        self.grid = defaultdict(lambda: None)
+        self.grid = defaultdict(lambda: self.empty_field)
 
     def is_legal_position(self, position):
         """Check whether position is a legal position on the board.
@@ -32,6 +42,13 @@ class Board:
 
         return True
 
+    def piece_to_one_hot(self, piece):
+        return self._one_hots[self.type_to_id[type(piece)]]
+
     def to_one_hot(self):
-        # TODO implement
-        return 0
+        one_hot_grid = np.zeros((*self.grid_size, self.one_hot_dim))
+        ranges = (range(x) for x in self.grid_size)
+        for vec in itertools.product(*ranges):
+            one_hot_grid[vec] = self.piece_to_one_hot(self.grid[vec])
+
+        return one_hot_grid
