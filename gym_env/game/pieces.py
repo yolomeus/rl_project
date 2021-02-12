d28bcff2b6fe2103702e7fd61ef274c415a1e0ba
Fabian <fab.beringer@gmail.com>
2021-02-12 18:13:25 +0100

    add position attribute and placement callback method

--- gym_env/game/pieces.py
+++ gym_env/game/pieces.py
@@ -1,15 +1,26 @@
+import itertools
 from abc import ABC, abstractmethod
+
+import numpy as np
 
 
 class Piece(ABC):
-    def __init__(self, player, board):
+    def __init__(self, player, board, position=None):
         self.age = 0
         self.player = player
         self.board = board
+        self.position = position
 
     @abstractmethod
-    def compute_reward(self):
+    def turn_reward(self):
         """Compute the current reward for the piece"""
+
+    def at_placement(self):
+        """Perform any effects that placement of this piece has."""
+        pass
+
+    def __str__(self):
+        return f'Player {self.player.player_id}: {self.__class__.__name__}'
 
 
 class Empty(Piece):
