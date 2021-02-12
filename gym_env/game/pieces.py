45ea1dbd9c986a4520d31fdc0661926fb30c4d37
Fabian <fab.beringer@gmail.com>
2021-02-12 18:14:10 +0100

    implement reward computation for pieces

--- gym_env/game/pieces.py
+++ gym_env/game/pieces.py
@@ -25,17 +25,36 @@ class Piece(ABC):
 
 class Empty(Piece):
 
-    def compute_reward(self):
+    def turn_reward(self):
         return 0
 
 
 class City(Piece):
-    # TODO implement
-    def compute_reward(self):
-        return 1
+    room_capacity = 1
+
+    def turn_reward(self):
+        return 0
+
+    def at_placement(self):
+        self.player.room += self.room_capacity
 
 
 class Farm(Piece):
-    # TODO implement
-    def compute_reward(self):
-        return 1
+    reward_size = 1
+    reward_delay = 0
+
+    def turn_reward(self):
+        # only produces a reward if adjacent to a city
+        if self.age >= self.reward_delay:
+            # get adjacent pieces
+            n_dims = len(self.position)
+            adjacent_directions = itertools.permutations((-1, 0, 1), n_dims)
+            for direction in adjacent_directions:
+                adj_pos = tuple(self.position + np.array(direction))
+                piece = self.board.get_piece(adj_pos)
+                if isinstance(piece, City) and piece.player == self.player:
+                    return self.reward_size
+        return 0
+
+    def at_placement(self):
+        self.player.population += 1
