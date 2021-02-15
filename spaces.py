9c79587f77a5da27cb1304353f16ce47e7fc2138
Fabian <fab.beringer@gmail.com>
2021-02-15 22:02:29 +0100

    add one-hot box space

--- spaces.py
+++ spaces.py
@@ -1,10 +1,11 @@
 import numpy as np
-from gym.spaces import MultiBinary
+from gym.spaces import MultiBinary, Box
 
 
 class OneHot(MultiBinary):
-    """n-dimensional space of one-hot vectors e.g. with shape (2, 3, 6), from which a sample is a tensor
-    containing 2 x 3 six-dimensional one-hot vectors.
+    """Special case of MultiBinary space, where each entry along the last axis is a one-hot vector. e.g. with shape
+    (2, 3, 6), from which a sample is a tensor containing 2 x 3 six-dimensional
+    one-hot vectors.
     """
 
     def __init__(self, shape):
@@ -36,3 +37,49 @@ class OneHot(MultiBinary):
         is_one_hot = has_single_element and sums_to_one
 
         return has_same_shape and is_one_hot
+
+
+class OneHotBox(Box):
+    """Concatenation of a OneHot and Box space.
+    """
+
+    def __init__(self, one_hot, flat_box, flatten=True):
+        self.one_hot = one_hot
+        self.flat_box = flat_box
+        self.flatten = flatten
+        if not flatten:
+            shape = list(one_hot.n)
+            shape[-1] = shape[-1] + flat_box.shape[0]
+            shape = tuple(shape)
+        else:
+            shape = (np.prod(one_hot.n) + flat_box.shape[0],)
+
+        super().__init__(0, 1, shape)
+
+    def sample(self):
+
+        obs_0, obs_1 = self.one_hot.sample(), self.flat_box.sample()
+
+        if not self.flatten:
+            obs_1 = np.repeat(obs_1, np.prod(obs_0.shape[:-1])).reshape(obs_0.shape[:-1] + (-1,))
+            x = np.concatenate([obs_0, obs_1], axis=-1)
+            return x
+        obs = np.concatenate([obs_0.ravel(), obs_1])
+        return obs
+
+    def contains(self, x):
+        n_one_hot = self.one_hot.one_hot_dim
+
+        if not self.flatten:
+            one_hot_obs = x[:, :n_one_hot].reshape(self.one_hot.n)
+            x = x.reshape((-1, self.shape[-1]))
+            box_obs = x[0, n_one_hot:]
+        else:
+            len_flat_box = self.flat_box.shape[0]
+            one_hot_obs = x[:-len_flat_box].reshape(self.one_hot.n)
+            box_obs = x[-len_flat_box:]
+
+        contains_one_hot = self.one_hot.contains(one_hot_obs)
+        contains_box = self.flat_box.contains(box_obs)
+
+        return contains_one_hot and contains_box
