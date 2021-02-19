f3cc02da65848656aa9014848082eaf3993a68d4
Fabian <fab.beringer@gmail.com>
2021-02-19 18:47:04 +0100

    implement some basic rendering

--- gym_env/game/pieces.py
+++ gym_env/game/pieces.py
@@ -2,6 +2,7 @@ import itertools
 from abc import ABC, abstractmethod
 
 import numpy as np
+from pyglet.shapes import Rectangle
 
 
 class Piece(ABC):
@@ -22,15 +23,29 @@ class Piece(ABC):
     def __str__(self):
         return f'Player {self.player.player_id}: {self.__class__.__name__}'
 
+    def to_drawable(self, x, y, batch, square_size, color):
+        r = Rectangle(x, y,
+                      square_size, square_size,
+                      color=color,
+                      batch=batch)
+        return r
+
 
 class Empty(Piece):
 
     def turn_reward(self):
         return 0
 
+    def to_drawable(self, x, y, batch, square_size, color):
+        r = Rectangle(x, y,
+                      square_size, square_size,
+                      color=(10, 10, 10),
+                      batch=batch)
+        return r
+
 
 class City(Piece):
-    room_capacity = 1
+    room_capacity = 0.5
 
     def turn_reward(self):
         return 0
@@ -38,23 +53,61 @@ class City(Piece):
     def at_placement(self):
         self.player.room += self.room_capacity
 
+    def to_drawable(self, x, y, batch, square_size, color):
+        shapes = []
+        r = super().to_drawable(x, y, batch, square_size, color)
+        shapes.append(r)
+
+        w = h = square_size * .4
+        offset = square_size * .5
+        r_inner = Rectangle(x + offset, y + offset,
+                            w, h,
+                            color=(0, 0, 0),
+                            batch=batch)
+        r_inner.anchor_position = (w * .5, h * .5)
+        r_inner.opacity = 128
+        shapes.append(r_inner)
+
+        return shapes
+
 
 class Farm(Piece):
     reward_size = 1
     reward_delay = 0
+
+    def __init__(self, player, board, ignore_diagonal=True):
+        super().__init__(player, board)
+        self.generates_reward = False
+        self.ignore_diagonal = ignore_diagonal
 
     def turn_reward(self):
         # only produces a reward if adjacent to a city
         if self.age >= self.reward_delay:
             # get adjacent pieces
             n_dims = len(self.position)
-            adjacent_directions = itertools.permutations((-1, 0, 1), n_dims)
+
+            if self.ignore_diagonal:
+                adjacent_directions = []
+                for i, val in itertools.product(range(n_dims), [-1, 1]):
+                    vec = np.zeros((n_dims,))
+                    vec[i] = val
+                    adjacent_directions.append(vec)
+            else:
+                adjacent_directions = itertools.product((-1, 0, 1), repeat=n_dims)
+
             for direction in adjacent_directions:
                 adj_pos = tuple(self.position + np.array(direction))
                 piece = self.board.get_piece(adj_pos)
                 if isinstance(piece, City) and piece.player == self.player:
+                    self.generates_reward = True
                     return self.reward_size
         return 0
 
     def at_placement(self):
         self.player.population += 1
+
+    def to_drawable(self, x, y, batch, square_size, color):
+        r = super().to_drawable(x, y, batch, square_size, color)
+        if not self.generates_reward:
+            r.opacity = 100
+        return r
---
+++ gym_env/rendering.py
@@ -0,0 +1,150 @@
+from random import randint
+
+import pyglet
+from pyglet.graphics import Batch
+from pyglet.shapes import Rectangle
+from pyglet.text import Label
+from pyglet.window import Window
+
+from gym_env.game.game import ExpandoGame
+from gym_env.game.pieces import Empty, Farm, City
+
+
+class GameRenderer(Window):
+    """Takes a 2D ExpandoGame and draws it, everytime `step()` is called.
+
+    """
+
+    def __init__(self, game: ExpandoGame, cell_size=50, padding=10, ui_font_size=12):
+        """
+
+        :param game: The game to render
+        :param cell_size: the length of each square in pixels
+        :param padding: the padding between cells in pixels
+        """
+        self.player_colors = [(84, 22, 180),
+                              (255, 106, 0),
+                              (204, 255, 0),
+                              (244, 147, 242)]
+        self.padding = padding
+        self.cell_size = cell_size
+        self.square_size = self.cell_size - self.padding
+        self.game = game
+        self.board = self.game.board
+        self.font_height = ui_font_size * 0.75
+
+        assert len(self.board.grid_size) == 2, 'only 2d grids can be rendered at the moment'
+
+        h, w = self.game.grid_size
+        window_height = h * cell_size + padding
+        # extra room for displaying scores
+        self.window_height = window_height + 2 * self.font_height * (game.n_players + 1) + self.padding
+        self.window_width = w * cell_size + padding
+
+        super().__init__(width=self.window_width, height=int(self.window_height))
+        self.batch = Batch()
+
+    @staticmethod
+    def step():
+        pyglet.clock.tick()
+
+        for window in pyglet.app.windows:
+            window.switch_to()
+            window.dispatch_events()
+            window.dispatch_event('on_draw')
+            window.flip()
+
+    def on_draw(self):
+        self.clear()
+        self.draw_grid()
+        self.draw_cursors()
+        self.draw_scores()
+
+    def draw_grid(self):
+
+        h, w = self.board.grid_size
+
+        pieces = []
+        for i in range(w):
+            for j in range(h):
+                piece = self.board.get_piece((j, i))
+                x, y = self._get_canvas_pos(i, j)
+                r = piece.to_drawable(x, y, self.batch, self.square_size, self._get_piece_color(piece))
+                pieces.append(r)
+
+        self.batch.draw()
+
+    def draw_cursors(self):
+        rects = []
+        for player in self.game.players:
+            cursor = tuple(player.cursor)
+            x, y = self._get_canvas_pos(*cursor)
+            color = self._get_player_color(player)
+            color = self.brighten(color, 50)
+            r = Rectangle(y, x,
+                          self.square_size, self.square_size,
+                          color=color,
+                          batch=self.batch)
+            rects.append(r)
+
+        self.batch.draw()
+
+    def draw_scores(self):
+        batch = Batch()
+        labels = []
+        header = ['pl', 'population/room', 'happiness', 'turn reward', 'total reward']
+        sep = ' | '
+        score_strings = [sep + sep.join(header) + sep]
+        for player in self.game.players:
+            scores = map(str, [player.player_id, f'{player.population}/{player.room}', player.happiness_penalty,
+                               player.current_reward, player.total_reward])
+            line = ''
+            for i, score in enumerate(scores):
+                score_len = len(str(score))
+                head_len = len(header[i])
+                line += ' ' * (head_len + len(sep) - score_len)
+                line += score
+            score_strings.append(line)
+
+        # .75 for pt to px
+        font_size = self.font_height / 0.75  # int(.75 * self.window_height * self.score_space / self.game.n_players)
+        for i, score_str in enumerate(score_strings, start=1):
+            if i > 1:
+                c = self._get_player_color(self.game.players[i - 2]) + (255,)
+                c = self.brighten(c, 50)
+            else:
+                c = (255,) * 4
+            label = Label(score_str,
+                          x=0,
+                          y=self.height - 2 * self.font_height * i,
+                          font_name='Consolas',
+                          font_size=font_size,
+                          color=c,
+                          batch=batch)
+            labels.append(label)
+        batch.draw()
+
+    def _get_canvas_pos(self, x, y):
+        return x * self.cell_size + self.padding, y * self.cell_size + self.padding
+
+    def _get_piece_color(self, piece):
+        return self._get_player_color(piece.player)
+
+    def _get_player_color(self, player):
+        if player is None:
+            return 10, 10, 10
+
+        return self.player_colors[player.player_id]
+
+    @staticmethod
+    def brighten(color, val):
+        new_color = []
+        for c in color:
+            x = c + val
+            if 0 <= x <= 255:
+                new_color.append(x)
+            elif x < 0:
+                new_color.append(0)
+            else:
+                new_color.append(255)
+        return new_color
