03fabb50af201960b5f62acf008b5f37e6d8a22d
Fabian <fab.beringer@gmail.com>
2021-02-24 18:00:27 +0100

    update game class docstrings

--- gym_env/game/game.py
+++ gym_env/game/game.py
@@ -10,14 +10,23 @@ from gym_env.game.player import Player
 
 
 class ExpandoGame:
-    def __init__(self, grid_size, n_players, max_turns, piece_types, final_reward, seed=None):
+    """Expando is a multiplayer, turn based, strategy game that takes place on a d_0 x ... x d_n grid.
+    Each turn, a player receives reward based on the constellation of all the pieces that she has placed on the board
+    so far. Every player is associated with a cursor which can be moved along all of the board's axis but only for 1
+    step per turn. In addition, a player may choose to set a piece after moving or choosing not to move.
+    The player with the largest total reward at the game's end, wins and receives a large reward, the loser wins the
+    same amount but as penalty.
+    """
+
+    def __init__(self, grid_size, n_players, max_turns, final_reward, piece_types, seed=None):
         """
 
-        :param grid_size:
-        :param n_players:
-        :param max_turns:
-        :param piece_types:
-        :param seed:
+        :param grid_size: the dimensions of the board.
+        :param n_players: number of players participating in the game.
+        :param max_turns: the maximum number of turns that a game is allowed to last. Each player's turn is counted.
+        :param piece_types: list of sub-classes of Piece, that can be used in the game
+        :param final_reward: the amount of reward that is either granted for winning or used as penalty for loosing
+        :param seed: used to seed any random number generators
         """
         self.np_random = default_rng(seed)
 
@@ -37,6 +46,8 @@ class ExpandoGame:
         self._action_pairs = None
 
     def _init_player_positions(self):
+        """Place each player's cursor at a random position.
+        """
         cursors = set()
         while len(cursors) < self.n_players:
             cursor = tuple(self.np_random.integers(0, self.grid_size))
@@ -46,6 +57,12 @@ class ExpandoGame:
             p.cursor = np.array(c, dtype=np.int64)
 
     def take_turn(self, action, player_id):
+        """Perform a player's turn given an action.
+
+        :param action: the action the paler should take, encoded as integer.
+        :param player_id: the player_id of the player that should perform the action.
+        :return: the player's reward after performing the action.
+        """
         if not isinstance(action, list):
             action = self._discrete_to_multidiscrete(action)
 
@@ -67,6 +84,7 @@ class ExpandoGame:
         self.n_turns += 1
 
         if self.is_done:
+            # add the final reward or penalty, depending on whether the player did win or lose
             other_players = [self.players[i] for i in range(len(self.players)) if i != player_id]
             if all([cur_player.total_reward > p.total_reward for p in other_players]):
                 # player has won
@@ -77,32 +95,49 @@ class ExpandoGame:
         return reward
 
     def reset(self):
+        """Reset the game's state and place the player's cursors at random positions.
+        """
         self.n_turns = 0
         self.board.reset_grid()
         self.players = [Player(i, self.board) for i in range(self.n_players)]
         self._init_player_positions()
 
     def get_observation(self, player_id, formatting):
-        """
+        """Return an observation from the perspective of a player, i.e. treating her as player 0.
 
-        :param player_id:
-        :param formatting: 'flat' or 'grid'
-        :return:
+        :param player_id: player_id of the player from who's perspective the game is observed.
+        :param formatting: 'flat' or 'grid' representation of the game. Where flat is a k-dimensional vector, and grid a
+        d_0 x ... x d_n dimensional tensor.
+        :return: the observation of the player encoded as numpy array.
         """
         return self.players[player_id].get_observation(formatting)
 
     @property
     def is_done(self):
+        """Whether the game has reached a terminal state.
+
+        :return: bool, true if game has ended
+        """
         return self.board.is_full() or self.n_turns > self.max_turns
 
     @property
     def all_pieces(self):
+        """Get a list of all pieces that are currently placed on the board.
+
+        :return: list of all pieces.
+        """
         all_pieces = []
         for player in self.players:
             all_pieces.extend(player.pieces)
         return all_pieces
 
     def _decode_action(self, action, action_type):
+        """Factory method for decoding integer encoded actions based on the type of action.
+
+        :param action: integer encoding an action
+        :param action_type: either 'cursor_move' ir 'piece_type'.
+        :return: a decoded action.
+        """
         if action_type == 'cursor_move':
             return self._decode_cursor_move(action)
         elif action_type == 'piece_type':
